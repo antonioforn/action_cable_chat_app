3bb58af5ba744dedbdc08baa6709bc506c91d2cf
Antonio Forneron <antonioforneron@gmail.com>
2016-12-15 10:50:13 -0300

    correct config.action_cable.url https to wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.url = 'https://tryingactioncable.herokuapp.com/cable'
+  config.action_cable.url = 'wss://tryingactioncable.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [
     'https://tryingactioncable.herokuapp.com/' ]
 end
